3735602c75ec83d42b884fbd2d7f74c911221815
WestleyK <westley@sylabs.io>
2018-07-11 23:08:39 +0000

    fixed install script

--- install.sh
+++ install.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 #
 # Created by: Westley K
-# Date: Jul 5, 2018
-# version-1.2
-# https://github.com/WestleyK/backlight-adjuster
+# Date: Jul 11, 2018
+# version-2.0
+# https://github.com/WestleyK/pi-backlight
 #
 # this install script must be runed as root or sudo
 # sudo ./install.sh
 
 
 INSTALL_PATH="/usr/local/bin"
+SCRIPT_NAME="pi-backlight"
 
 
 # check if your root
@@ -23,29 +24,29 @@ option=$1
 if [[ -n $option ]]; then
 	case $option in
 		-h | -help | --help)
-			echo "usage ./install [option]
+			echo "Usage: sudo ./install [option]
 		-h | -help | --help (display help menu)
 		-c (check if on latest version) (comming soon)
 		-u (un-install command)"
 			exit
 			;;
 		-c)
-			echo "comming soon!"
+			echo "Comming soon!"
 			exit
 			;;
 		-u)
 			un_install=$"true"
-			echo "un-installing"
+			echo "Un-installing"
 			;;
 		*)
-			echo "option not found :o try: ./install.sh -help"
+			echo "Option not found :o try: ./install.sh -help"
 			;;
 	esac
 fi
 
 
-echo "backlight-adjust installer"
-echo "https://github.com/WestleyK/backlight-adjust"
+echo "pi-backlight installer"
+echo "https://github.com/WestleyK/pi-backlight"
 echo "./install.sh -help (for help)"
 echo
 
@@ -54,31 +55,31 @@ echo
 if [[ $un_install == "true" ]]; then
 	check_script=$( ls $INSTALL_PATH | grep backlight-adjust )
 	if [[ -z $check_script ]]; then
-		echo "nothing to un-install"
+		echo "Nothing to un-install"
 		exit
 	fi
-	echo "are you sure you want to un-install backlight-adjust"
+	echo "Are you sure you want to un-install $SCRIPT_NAME"
 	echo -n "[y,n]:"
 	read input
 	echo
 	if [[ $input == "y" || $input == "Y" ]]; then
-		echo "un-installing..."
-		sudo rm $INSTALL_PATH/backlight-adjust
-		echo "un-installed"
+		echo "Un-installing..."
+		sudo rm $INSTALL_PATH/$SCRIPT_NAME
+		echo "Un-installed"
 		exit
 	fi
 fi
 
 
 # check if its installed aready
-check_script=$( ls $INSTALL_PATH | grep backlight-adjust )
+check_script=$( ls $INSTALL_PATH | grep $SCRIPT_NAME )
 if [[ -n $check_script ]]; then 
-	echo "it seems like its already installed"
+	echo "It seems like its already installed"
 	exit
 fi
 
 # check if the script is still here
-check_script=$( ls | grep ^backlight-adjust$ )
+check_script=$( ls | grep ^$SCRIPT_NAME$ )
 if [[ -z $check_script ]]; then 
 	echo "No script to install."
 	exit
@@ -86,16 +87,16 @@ fi
 echo "Installing..."
 
 # the install part
-sudo chmod +x backlight-adjust
-sudo cp backlight-adjust $INSTALL_PATH
+sudo chmod +x $SCRIPT_NAME
+sudo cp $SCRIPT_NAME $INSTALL_PATH
 
 echo "Installed!"
 echo
-echo "(backlight-adjust) is installed!"
-echo "See: backlight-adjust -help (for help)"
+echo "($SCRIPT_NAME) is installed!"
+echo "See: $SCRIPT_NAME -help (for help)"
 
 
 #
-# End install script
+# End Install Script
 #
 
