44211651eaea30a31c985b2019e47978d36ce3dd
WestleyK <westley@sylabs.io>
2018-07-19 01:31:07 +0000

    changed to sh script and update option

--- install.sh
+++ install.sh
@@ -1,102 +1,95 @@
-#!/bin/bash
+#!/bin/sh
 #
 # Created by: Westley K
-# Date: Jul 11, 2018
-# version-2.0
-# https://github.com/WestleyK/pi-backlight
-#
-# this install script must be runed as root or sudo
-# sudo ./install.sh
+# email: westley@sylabs.io
+# Date: Jul 18, 2018
+# version-1.0-beta-1
 
 
-INSTALL_PATH="/usr/local/bin"
+OPTION=$1
 SCRIPT_NAME="pi-backlight"
+PATH_INSTALL="/usr/local/bin/"
 
 
-# check if your root
-if [[ "$EUID" -ne 0 ]]; then 
-	echo "Please run as root"
-	echo "sudo ./install.sh"
+IS_ROOT=$( id -u )
+
+if [ $IS_ROOT != 0 ]; then
+	echo "Please run as root!"
+	echo "try: sudo ./install.sh"
 	exit
 fi
 
-option=$1
-if [[ -n $option ]]; then
-	case $option in
-		-h | -help | --help)
-			echo "Usage: sudo ./install [option]
-		-h | -help | --help (display help menu)
-		-c (check if on latest version) (comming soon)
-		-u (un-install command)"
+
+if [ -z $OPTION ]; then
+	if [ -e $PATH_INSTALL$SCRIPT_NAME ]; then
+		echo $SCRIPT_NAME "is already installed to" $PATH_INSTALL
+		echo "Continuing will overide the existing script"
+		echo "Do you want to continue?"
+		echo -n "[y,n]:"
+		read INPUT
+		if [ $INPUT = "y" ] || [ $INPUT = "Y" ]; then
+			echo "Installing" $SCRIPT_NAME\ "..."
+			chmod +x pi-backlight
+			cp $SCRIPT_NAME $PATH_INSTALL
+			echo "Installed to" $PATH_INSTALL
 			exit
-			;;
-		-c)
-			echo "Comming soon!"
-			exit
-			;;
-		-u)
-			un_install="true"
-			echo "Un-installing"
-			;;
-		*)
-			echo "Option not found :o try: ./install.sh -help"
-			;;
-	esac
+		fi
+	fi
+
+	echo "Installing" $SCRIPT_NAME\ "..."
+	chmod +x $SCRIPT_NAME
+	cp $SCRIPT_NAME $PATH_INSTALL
+	echo "Installed to" $PATH_INSTALL
+	exit
+
 fi
 
-
-echo "pi-backlight installer"
-echo "https://github.com/WestleyK/pi-backlight"
-echo "./install.sh -help (for help)"
-echo
-
-
-# check if we need to un-install
-if [[ $un_install == "true" ]]; then
-	check_script=$( ls $INSTALL_PATH | grep $SCRIPT_NAME )
-	if [[ -z $check_script ]]; then
-		echo "Nothing to un-install"
-		exit
-	fi
-	echo "Are you sure you want to un-install $SCRIPT_NAME"
+if [ "$OPTION" = "-u" ] || [ "$OPTION" = "-update" ]; then
+	echo "This will update the existing script if theres one present"
+	echo "Are you sure you want to continue?"
 	echo -n "[y,n]:"
-	read input
-	echo
-	if [[ $input == "y" || $input == "Y" ]]; then
-		echo "Un-installing..."
-		sudo rm $INSTALL_PATH/$SCRIPT_NAME
-		echo "Un-installed"
+	read INPUT
+	if [ $INPUT = "y" ] || [ $INPUT = "Y" ]; then
+		echo "Updating..."
+		git pull origin master
+		echo "Installing" $SCRIPT_NAME\ "..."
+		chmod +x $SCRIPT_NAME
+		cp $SCRIPT_NAME $PATH_INSTALL
+		echo "Installed to" $PATH_INSTALL
+		exit
+	else
+		echo "Aborting."
 		exit
 	fi
-fi
-
-
-# check if its installed aready
-check_script=$( ls $INSTALL_PATH | grep $SCRIPT_NAME )
-if [[ -n $check_script ]]; then 
-	echo "It seems like its already installed"
+elif [ "$OPTION" = "-r" ] || [ "$OPTION" = "-uninstall" ]; then
+		if [ -e $PATH_INSTALL$SCRIPT_NAME ]; then
+			echo "This will uninstall" $SCRIPT_NAME "from" $PATH_INSTALL
+			echo "Are you sure you want to continue?"
+			echo -n "[y,n]:"
+			read INPUT
+			if [ $INPUT = "y" ] || [ $INPUT = "Y" ]; then
+				echo "Uninstalling" $SCRIPT_NAME "from" $PATH_INSTALL
+				rm $PATH_INSTALL$SCRIPT_NAME
+				echo "Done."
+				exit
+			else
+				echo "Aborting."
+				exit
+			fi
+		else
+			echo \'$SCRIPT_NAME\' "not installed to" \'$PATH_INSTALL\'
+			exit
+		fi
+else
+	echo "Usage: sudo ./install [OPTION]
+		-u | -update (update command and repo)
+		-r | -uninstall (uninstall command)"
 	exit
 fi
 
-# check if the script is still here
-check_script=$( ls | grep $SCRIPT_NAME )
-if [[ -z $check_script ]]; then 
-	echo "No script to install."
-	exit
-fi
-echo "Installing..."
-
-# the install part
-sudo chmod +x $SCRIPT_NAME
-sudo cp $SCRIPT_NAME $INSTALL_PATH
-
-echo "Installed!"
-echo
-echo "($SCRIPT_NAME) is installed!"
-echo "See: $SCRIPT_NAME -help (for help)"
-
 
 #
-# End Install Script
+# End install script
 #
+
 
